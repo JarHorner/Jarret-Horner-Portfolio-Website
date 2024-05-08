fac01256aed54fe60cdad37a6ae8c6eb0ef444c7
Jarret Horner <JHorn384@mtroyal.ca>
2024-05-08 12:38:27 -0600

    responsive project images

--- src/components/Helpers/ImageContainer.jsx
+++ src/components/Helpers/ImageContainer.jsx
@@ -4,7 +4,7 @@ import React from "react";
 const ImageContainer = ({ image }) => {
   return (
     <div className="flex items-center justify-center">
-      <div className="mt-8 h-[600px] w-[900px] overflow-hidden">
+      <div className="mt-8 h-[325px] w-[275px] sm:h-[400px] sm:w-[450px] md:h-[450px] md:w-[500px] lg:h-[500px] lg:w-[650px] xl:h-[550px] xl:w-[800px] 2xl:h-[600px] 2xl:w-[900px] overflow-hidden">
         <img src={image} alt={`Image ${image}`} className="w-full h-full object-contain"/>
       </div>
     </div>
